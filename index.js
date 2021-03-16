9e959841bcbb89668ddd01948085785c20179c41
wenxiu.su <wenxiu.su@beibei.com>
2021-03-16 21:27:18 +0800

    11

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const Koa = require('koa');
-const app = new Koa();
+const Koa = require('koa')
+const app = new Koa()
 
-app.use((ctx, next) => {
-    ctx.body = {
-        name: 'tom',
-    }
-});
-
-app.listen(3000, () => {
-    console.log('koa start')
+app.use( async ( ctx ) => {
+    console.log('111')
+    console.log(ctx)
+    let url = ctx.request.url
+    ctx.body = url
+    // ctx.body = {
+    //     name: 'tom',
+    // }
 })
+app.listen(3000)
