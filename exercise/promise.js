35cd491956f484344d94e45195f32194f382be07
wenxiu.su <wenxiu.su@beibei.com>
2021-04-26 23:24:38 +0800

    promise all 手动实现

--- exercise/promise.js
+++ exercise/promise.js
@@ -29,7 +29,7 @@
 // }
 
 
-
+/***********************************************/
 // Promise.all()的实现
 
 let p1 = new Promise((resolve, reject) => {
@@ -44,8 +44,41 @@ let p2 = new Promise((resolve, reject) => {
     }, 2000);
 })
 
+// console.time('cost')
+// Promise.all([p1, p2]).then(res => {
+//     console.log(res);
+//     console.timeEnd('cost')
+// });
+
+Promise.all2 = function (promises) {
+    return new Promise((resolve, reject) => {
+
+        let result = [];
+        let index = 0;
+        let len = promises.length;
+
+        if (!len) {
+            resolve(result);
+            return;
+        }
+
+        for (let i=0; i<len; i++) {
+            // 为什么不直接 promises[i].then, 因为promises[i]可能不是一个promise
+            Promise.resolve(promises[i]).then(data => {
+                result.push(data);
+                index++;
+                if (index === len) {
+                    resolve(result);
+                }
+            }).catch(err => {
+                reject(err)
+            })
+        }
+    });
+}
+
 console.time('cost')
-Promise.all([p1, p2]).then(res => {
+Promise.all2([p1, p2]).then(res => {
     console.log(res);
     console.timeEnd('cost')
 });
